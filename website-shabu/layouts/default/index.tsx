36525f7ef54fbd6ed8213a633338c6e7e453cf80
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-08-29 00:23:58 +0700

    style: make a full height content with fixed nav and footer

--- website-shabu/layouts/default/index.tsx
+++ website-shabu/layouts/default/index.tsx
@@ -1,15 +1,33 @@
 import { Layout, Menu } from "antd"
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Navbar from "../../components/navbar";
 
 const { Footer, Content } = Layout;
 
 
 const DefaultLayout = (props: any) => {
+    const [navHeight, setNavHeight] = useState(0)
+    const [footerHeight, footerNavHeight] = useState(0)
+    const navRef: any = useRef(null)
+    const footerRef: any = useRef(null)
+    useEffect(() => {
+        setNavHeight(navRef.current.clientHeight)
+        footerNavHeight(footerRef.current.clientHeight)
+    })
     return (
         <Layout>
-            <Navbar />
-            <Content>{props.children}</Content>
-            <Footer>Footer</Footer>
+            <div ref={navRef}>
+                <Navbar />
+            </div>
+            <div style={{
+                height: `calc(100vh - ${navHeight + footerHeight}px)`,
+                overflow: "auto"
+            }}>
+                <Content>{props.children}</Content>
+            </div>
+            <div ref={footerRef}>
+                <Footer>Footer</Footer>
+            </div>
         </Layout>
     )
 }
