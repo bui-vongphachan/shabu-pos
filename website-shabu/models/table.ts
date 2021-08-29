15650412f8b24d66cbd8cd9af54d54afe88f1606
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-08-29 13:25:48 +0700

    feat: add add table and table list component
    user are now be able to create a table and see list of tables update in realtime

---
+++ website-shabu/models/table.ts
@@ -0,0 +1,6 @@
+export interface TableModel {
+    id: String
+    name: String
+    created_date: String
+    schema_version: number
+}
--- website-shabu/pages/table/index.tsx
+++ website-shabu/pages/table/index.tsx
@@ -1,13 +1,82 @@
+import { useMutation } from "@apollo/client"
+import { Button, Card, Form, Input, } from "antd"
+import { useState } from "react"
 import DefaultLayout from "../../layouts/default"
+import { client } from "../../lib/apolloSetup"
+import { addTableQueryString } from "../../lib/graphql/addtable.gql"
+import { getTablesQueryString } from "../../lib/graphql/getTable.gql"
+import { PageProps, TableModel } from "../../models"
+import dynamic from 'next/dynamic'
+import { useEffect } from "react"
+import { ValidateStatus } from "antd/lib/form/FormItem"
 
-const Table = () => {
+const TableList = dynamic(() => import('./tableList'),
+    { loading: () => <p>...</p> })
+
+
+const TablePage = (props: PageProps) => {
+    const [name, setName] = useState("")
+    const [validation, setValidation] = useState<ValidateStatus>("")
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const [tables, saveTables] = useState<TableModel[]>([])
+    const [addTable, addTableMutation] = useMutation(addTableQueryString);
+
+    const submitForm = async () => {
+        try {
+            setValidation("validating")
+            const response = await addTable({ variables: { addTableName: name } })
+
+            saveTables(response.data.addTable)
+            setName("")
+            setValidation("")
+
+        } catch (error) {
+
+        }
+    }
+    useEffect(() => {
+        saveTables(props.tables)
+    }, [])
+
+    useEffect(() => {
+        if (addTableMutation.error) {
+            setValidation("error")
+            setErrorMessage(addTableMutation.error.message)
+        }
+    }, [addTableMutation.error])
     return (
-        <div>
-            TABLE
+        <div className=" container m-auto">
+            <div className=" grid md:grid-cols-2 sm:grid-cols-1 justify-center">
+                <div>
+                    <Card className=" rounded-md m-3" title="ເພີ່ມໂຕະ">
+                        <Form requiredMark labelCol={{ span: 5 }}>
+                            <Form.Item validateStatus={validation} label="ເລກໂຕະ / ຊື່ໂຕະ" hasFeedback={errorMessage !== null} required help={errorMessage}>
+                                <Input disabled={addTableMutation.loading} value={name} onChange={e => setName(e.target.value)} />
+                            </Form.Item>
+                            <Form.Item wrapperCol={{ offset: 5, span: 5 }}>
+                                <Button disabled={name === "" || addTableMutation.loading} onClick={submitForm} >ເພີ່ມ</Button>
+                            </Form.Item>
+                        </Form>
+                    </Card>
+                </div>
+                <TableList tables={tables} />
+            </div>
         </div>
     )
 }
 
-Table.getLayout = DefaultLayout
+export const getStaticProps = async () => {
+    const { data } = await client.query({
+        query: getTablesQueryString,
+    });
+    return {
+        props: {
+            tables: data.getTables
+        }
+    }
+}
 
-export default Table
+export default TablePage
+
+
+TablePage.getLayout = DefaultLayout
