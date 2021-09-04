8748f65a300f4ae9f2c02a125662b5fac4db3398
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-09-04 20:13:43 +0700

    feat: use classnames to highlight busy tables

--- website-shabu/pages/report/index.tsx
+++ website-shabu/pages/report/index.tsx
@@ -1,10 +1,10 @@
 import { Card, List, Skeleton } from "antd"
 import DefaultLayout from "../../layouts/default"
-import { client } from "../../lib/apolloSetup"
-import Link from "next/link"
 import { GetReadyTablesQueryString, ReadyTableModel } from "../../models"
 import { useQuery } from "@apollo/client"
 import { useRouter } from "next/router"
+import Title from "antd/lib/typography/Title"
+import classnames from "classnames"
 
 const Report = () => {
     const { loading, error, data } = useQuery(GetReadyTablesQueryString)
@@ -46,10 +46,16 @@ const Report = () => {
                                 router.push(link)
                             }}>
                             <Card
-                                style={{ cursor: "pointer" }}
+                                style={{ cursor: "pointer", height: 200 }}
                                 className="cursor-pointer hover:shadow-lg p-3 "
-                                title={`${item.table.name}`}>
-                                {`${item.status}`}
+                                title={`${item.table.name}`}
+                            >
+                                <Title level={5}>
+                                    <span className={classnames({ "text-indigo-500": item.status !== "ວ່າງ" })}>
+                                        {item.status}
+                                    </span>
+                                </Title>
+                                {item.status !== "ວ່າງ" && <span  className={classnames({ "text-indigo-500": item.status !== "ວ່າງ" })}>ຈຳນວນອໍເດີ້: {item.orders}</span>}
                             </Card>
                         </a>
                     </List.Item>
