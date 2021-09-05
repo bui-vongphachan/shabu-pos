45b3f0f64c6798078827559d7d0e68a98934a5e1
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-09-05 23:54:20 +0700

    refactor: response boolean to suiting queries

--- backend-shabu/typeDefs/order.typedef.ts
+++ backend-shabu/typeDefs/order.typedef.ts
@@ -2,50 +2,39 @@ import { gql } from "apollo-server";
 
 export const orderTypeDef = gql`
   type OrderProductSize {
-    id: ProductSize,
-    name: String,
-    price: Float,
-    created_date: Date
-  },
-  type Order {
-    id: ID,
-    product: Product,
+    id: ProductSize
     name: String
-    isReceived: Boolean,
-    size: OrderProductSize,
-    quantity: Int,
-    totalPrice: Float,
-    isPaid: Boolean,
-    isDeleted: Boolean,
-    ordered_date: Date,
+    price: Float
+    created_date: Date
+  }
+  type Order {
+    id: ID
+    product: Product
+    name: String
+    isReceived: Boolean
+    size: OrderProductSize
+    quantity: Int
+    totalPrice: Float
+    isPaid: Boolean
+    isDeleted: Boolean
+    ordered_date: Date
     schema_version: Int
-  },
+  }
   input addOrderToInvoiceInput {
-    id: ID,
-    quantity: Int,
-    size: ID,
-  },
+    id: ID
+    quantity: Int
+    size: ID
+  }
   type Query {
     getOrder(order_id: ID): Order
-  },
+  }
   type Mutation {
     addOrderToInvoice(
-      invoice_id: ID,
-      products: [addOrderToInvoiceInput],
-    ): Invoice,
-    updateOrderQuantity(
-      invoice_id: ID,
-      order_id: ID,
-      quantity: Int
-    ): Invoice,
-    changeOrderSize(
       invoice_id: ID
-      order_id: ID
-      size_id: ID
-    ): Invoice,
-    deleteOrder(
-      invoice_id: ID
-      order_id: ID
-    ): Invoice,
+      products: [addOrderToInvoiceInput]
+    ): Invoice
+    updateOrderQuantity(invoice_id: ID, order_id: ID, quantity: Int): Boolean
+    changeOrderSize(invoice_id: ID, order_id: ID, size_id: ID): Boolean
+    deleteOrder(invoice_id: ID, order_id: ID): Invoice
   }
 `;
