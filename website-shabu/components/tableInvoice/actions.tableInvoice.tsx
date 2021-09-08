fc347e0e5dc0c5a01b0a52cba190a1d769696633
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-09-09 00:34:14 +0700

    feat: add ne order page

--- website-shabu/components/tableInvoice/actions.tableInvoice.tsx
+++ website-shabu/components/tableInvoice/actions.tableInvoice.tsx
@@ -1,23 +1,25 @@
+import { useRouter } from "next/router";
 import { useContext } from "react";
 import { TableInvoiceContext } from "../../pages/report/[table_id]";
 
 const ActionsTableInvoiceComponent = () => {
+  const router = useRouter();
   const tableInvoiceContext = useContext(TableInvoiceContext);
-  const { table_id } = tableInvoiceContext;
+  const { table_id, invoice } = tableInvoiceContext;
   return (
     <ul className="rounded-md py-3 flex flex-wrap md:justify-start justify-center">
       {[
         {
           label: "ສັ່ງອາຫານ",
-          link: `report/${table_id}/new-order`
+          link: `${table_id}/${invoice?.id}/new-order`
         },
-        { label: "ພິມບິນ", link: `report/${table_id}/bill` },
-        { label: "ຈ່າຍເງິນ", link: `report/${table_id}/pay` },
+        { label: "ພິມບິນ", link: `${table_id}/${invoice?.id}/bill` },
+        { label: "ຈ່າຍເງິນ", link: `${table_id}/${invoice?.id}/pay` },
         {
           label: "ຍ້າຍໂຕະ",
-          link: `report/${table_id}/switch-table`
+          link: `${table_id}/switch-table`
         },
-        { label: "ຍົກເລີກ", link: `report/${table_id}/cancel` }
+        { label: "ຍົກເລີກ", link: `${table_id}/${invoice?.id}/cancel` }
       ].map((item, index) => {
         return (
           <li style={{ margin: 1 }} key={index}>
@@ -26,6 +28,7 @@ const ActionsTableInvoiceComponent = () => {
                 type="button"
                 className="ant-btn rounded-md ant-btn-lg"
                 ant-click-animating-without-extra-node="false"
+                onClick={() => router.push(item.link)}
               >
                 <span>{item.label}</span>
               </button>
