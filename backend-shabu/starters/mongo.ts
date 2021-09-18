7fc6db2601439f1640109616203b14ff241caa0a
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-09-18 19:53:26 +0700

    refactor: use env

--- backend-shabu/starters/mongo.ts
+++ backend-shabu/starters/mongo.ts
@@ -2,15 +2,12 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB = async () => {
   return mongoose
-    .connect(
-      "mongodb+srv://admin:123@cluster0.2lo7y.mongodb.net/shabu?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-      }
-    )
+    .connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
     .then(() => console.log("DATABASE CONNECTED!!!"))
     .catch((er) => console.log(er));
 };
