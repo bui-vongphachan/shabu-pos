1bb4124914d0ed332d885da12c2169cc70dab817
Vongphachan Nukkalash <vongphachannukkalash@gmail.com>
2021-09-18 19:44:35 +0700

    fix: recorrect serving folder

--- website-shabu/serve/serve.js
+++ website-shabu/serve/serve.js
@@ -4,25 +4,25 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "out")));
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "out", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/orders", function (req, res) {
-  res.sendFile(path.join(__dirname, "out", "orders.html"));
+  res.sendFile(path.join(__dirname, "orders.html"));
 });
 
 app.get("/menu", function (req, res) {
-  res.sendFile(path.join(__dirname, "out", "menu.html"));
+  res.sendFile(path.join(__dirname, "menu.html"));
 });
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "out", "404.html"));
+  res.sendFile(path.join(__dirname, "404.html"));
 });
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log({ __dirname });
